6c11e3c967acc358b7934806f8cdee4f81c210f6
Pardeep Sahota <PS037@atcltd.atc.org.nz>
2021-04-01 13:35:48 +1300

    Add project files.

---
+++ Data/StaffWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StaffWebApi.Models;
+
+namespace StaffWebApi.Data
+{
+    public class StaffWebApiContext : DbContext
+    {
+        public StaffWebApiContext (DbContextOptions<StaffWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StaffWebApi.Models.StaffName> StaffName { get; set; }
+    }
+}
